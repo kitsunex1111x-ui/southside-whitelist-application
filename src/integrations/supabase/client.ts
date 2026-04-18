5c6c30d71e5ee904101ac78e3393bfe9703c8e6d
mejri0 <mejriiyed513@gmail.com>
2026-04-18 22:37:34 +0100

    fix: change auth flowType to pkce for Discord OAuth

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -10,6 +10,6 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     detectSessionInUrl: false,
     persistSession: true,
     autoRefreshToken: true,
-    flowType: "implicit",
+    flowType: "pkce",
   },
 });
